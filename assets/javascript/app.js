5c638872cb52b0797784cbcfb03799638310869b
QuantumT95 <tran.charlie@ymail.com>
2017-07-18 17:56:03 -0500

    final v1

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,30 +1,62 @@
+var questions = {
+	question1: 'honey',
+	question2: 'blue',
+	question3: '1492',
+	question4: 'austin',
+	question5: '8'
+};
+var time = 10;
+var correct = 0;
+var incorrect = 0;
 
 
+function check(event) {
+	clearInterval(window.timer); // stop countdown
 
-
-
-
-function check(){
+	if ( event ) event.preventDefault(); // prevent window from refreshing
 	
-	var question1 = document.quiz.question1.value;
-	var question2 = document.quiz.question2.value;
-	var question3 = document.quiz.question3.value;
-	var question4 = document.quiz.question4.value;
-	var correct = 0;
+	var question_amount = Object.keys(questions).length; // how many questions there are
+	var answered = $('form input:checked').length; // how many the user answered
+	var inputs = $('form input:checked');
+	// var arr = [0, 1, 2, 7, 4]
 
-		if (question1 == "honey") {
-			correct++;
-		}
-		if (question2 == "honey") {
-			correct++;
-		}
-		if (question3 == "honey") {
-			correct++;
-		}
-		if (question4 == "honey") {
-			correct++;
-		}
+	// console.log(arr[3]) // 7
+	// console.log(questions['question2']);
+	inputs.each(function() {
+		var question = $(this).attr('name');
 
-	document.getElementById("after-submit").style.visibility = "visible";
-	document.getElementById("number-correct").innerHTML = "You got " + correct + " correct!";
+		if ( questions[question] == $(this).val() ) {
+			correct++;
+		} else incorrect++;
+	});
+
+	$('#after-submit p').text('You answered ' + answered + ' questions. You got ' + correct + ' questions right and ' + incorrect + ' wrong.');
+	$('#after-submit').show();
+	$('form').hide();
 }
+
+function startCountDown() {
+	// intervals, timeouts
+	window.timer = setInterval(function() {
+		time--;
+
+		if ( time < 10 ) {
+			$('#display').text('00:0' + time);
+		} else $('#display').text('00:' + time);
+		
+		// 0, NaN, undefined, false, ect
+		if ( !time ) {
+			// time has run out
+			check();
+		}
+	}, 1000);
+}	
+
+function startGame() {
+	$(this).hide();
+	$('form, #display').show();
+	startCountDown();
+}
+
+$('#submit').on('click', check);
+$('#start').on('click', startGame);
